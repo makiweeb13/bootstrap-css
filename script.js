e702a00b19e41cf86750d4c850d4698195a3dd24
makiweeb13 <mariamaekiskis13@gmail.com>
2023-02-17 23:59:26 +0800

    first two buttons completed

---
+++ script.js
@@ -0,0 +1,33 @@
+const shuffleBtn = document.querySelector('.shuffle-btn');
+const addTextBtn = document.querySelector('.add-text-btn');
+const imgDisplay = document.querySelector('.img-display');
+const paragraph = document.querySelector('.paragraph');
+
+let images = [
+            'images/00wednesday-dance-superJumbo.jpg', 
+            'images/jenna.jpg', 
+            'images/piranha.jpg',
+            'images/wednesday-family.jpg',
+            'images/tylerXwednesday.jpg'
+            ];
+
+let charCount = 0;
+
+function shuffleImg() {
+    if (images.length > 0) {
+        const getRandomImg = images[Math.floor(Math.random()*images.length)];
+        imgDisplay.src = getRandomImg;
+        images = images.filter(img => getRandomImg !== img);
+    }   
+}
+
+shuffleBtn.addEventListener('click', () => shuffleImg());
+
+addTextBtn.addEventListener('click', () => {
+    if (charCount <= 1000) {
+        let input = document.getElementById('input-text').value;
+        paragraph.innerHTML += `<p>${input}</p>`;
+        charCount += input.length;
+        input = '';   
+    }
+});
