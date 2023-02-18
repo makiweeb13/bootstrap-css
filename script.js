6777dda38c6498a91feb7377650800546ef4fc80
makiweeb13 <mariamaekiskis13@gmail.com>
2023-02-19 00:01:21 +0800

    button activities completed

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const shuffleBtn = document.querySelector('.shuffle-btn');
 const addTextBtn = document.querySelector('.add-text-btn');
 const imgDisplay = document.querySelector('.img-display');
 const paragraph = document.querySelector('.paragraph');
+const find = document.querySelector('.find');
+const replaceWith = document.querySelector('.replace-with');
+const replaceBtn = document.querySelector('.replace');
 
 let images = [
             'images/00wednesday-dance-superJumbo.jpg', 
@@ -24,12 +27,26 @@ function shuffleImg() {
 function appendText() {
     if (charCount <= 1000) {
         let input = document.getElementById('input-text').value;
-        paragraph.innerHTML += input;
+        paragraph.textContent += ` ${input}`;
         charCount += input.length;   
         document.getElementById('input-text').value = '';   
     }
 }
 
+function findAndReplace() {
+    let paragraph2 = document.querySelector('.center-par').textContent.split(' ');
+    for (let i = 0; i < paragraph2.length; i++) {
+        if (paragraph2[i] === find.value) {
+            paragraph2[i] = replaceWith.value;
+        }
+    }
+    find.value = '';
+    replaceWith.value = '';
+    document.querySelector('.center-par').textContent = paragraph2.join(' ');
+}
+
 shuffleBtn.addEventListener('click', () => shuffleImg());
 
 addTextBtn.addEventListener('click', () => appendText());
+
+replaceBtn.addEventListener('click', () => findAndReplace());
